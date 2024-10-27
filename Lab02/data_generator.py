6a5fef4808ccb3bc46c69cfe27f2a107353318c6
JakubBron <jakub.bronowski1@gmail.com>
2024-10-27 13:17:18 +0100

    Generator done

--- Lab02/data_generator.py
+++ Lab02/data_generator.py
@@ -1,79 +1,79 @@
 from faker import Faker
 import random
 import datetime
-import json
+import os
 import string
 import csv
 
 # initialize faker
 
 faker = Faker('pl_PL')
-# DB and implementation specific
+# database and implementation specific constants
 gabaryty_maxTextLen = 20
 zlecenia_maxTextLen = 250
 our_seed = 2137
 
-# data specific
+# data specific params / dictionaries
 chance_pno = 0.7
 chance_companyPerson = 0.5
-chance_truckMalfunction = 0.4
+chance_truckMalfunction = 0.6
 timeStartDate = datetime.datetime(2020, 1, 1)
 timeEndDate = datetime.datetime(2024, 10, 25)
 timeEndDate_T2 = datetime.datetime(2025, 8, 10)
 fuelTypesList = ['ON', 'PB95', 'LPG']
-numberOfTrucks = 10
 maxMalfunctionsPerTruck = 5
 maxUpdated = 5
 
-
+# setting RNGs
 Faker.seed(our_seed)
 random.seed(our_seed)
 
-
-def generate_gabaryty(number, id=0):
+def generate_gabaryty(number, id_gabarytu = 0):
     dict_gabaryty = []
     for i in range(number):
-        id = id
         description = faker.text(gabaryty_maxTextLen)
         max_dim_x = random.randint(1, 9999)
         max_dim_y = random.randint(1, 9999)
         max_dim_z = random.randint(1, 9999)
         markup = round(random.uniform(5, 150), 2)
         
-        dict_gabaryty.append( { 'ID_gabarytu': id, 
+        dict_gabaryty.append( { 'ID_gabarytu': id_gabarytu, 
                                 'Opis': description,
                                 'Wymiar_X': max_dim_x,
                                 'Wymiar_Y': max_dim_y,
                                 'Wymiar_Z': max_dim_z,
                                 'Marza': markup} )
+        id_gabarytu += 1
     
-    return dict_gabaryty
+    return dict_gabaryty, id_gabarytu
 
-def generate_placowki(number_pno, number_dyst, id=0):
+def generate_placowki(number_pno, number_dyst, id_placowki=0):
     dict_placowki = []
     for i in range(number_pno):
-        id = id
         address = faker.address().replace('\n', ' ')
-        dict_placowki.append({'ID_placowki': id, 'Adres_placowki': address, 'Is_centrum_dystrybucyjne': False})
+        dict_placowki.append({'ID_placowki': id_placowki, 'Adres_placowki': address, 'Is_centrum_dystrybucyjne': False})
+        id_placowki += 1
+
     for i in range(number_dyst):
-        id = id + number_pno
         address = faker.address().replace('\n', ' ')
-        dict_placowki.append({'ID_placowki': id, 'Adres_placowki': address, 'Is_centrum_dystrybucyjne': True})
-    return dict_placowki
+        dict_placowki.append({'ID_placowki': id_placowki, 'Adres_placowki': address, 'Is_centrum_dystrybucyjne': True})
+        id_placowki += 1
+    
+    return dict_placowki, id_placowki
 
 def generate_statusy():
+    id_statusu = 4
     dict_statusy = [
         {'ID_statusu': 0, 'Opis_statusu': 'Przyjęte'},
         {'ID_statusu': 1, 'Opis_statusu': 'W trakcie realizacji'},
         {'ID_statusu': 2, 'Opis_statusu': 'Zakończone'},
         {'ID_statusu': 3, 'Opis_statusu': 'Anulowane'}
     ]
-    return dict_statusy
+    return dict_statusy, id_statusu
 
-def generate_zlecenia(number, dict_placowki, dict_gabaryty, dict_statusy, id=0):
+def generate_zlecenia(number, dict_placowki, dict_gabaryty, dict_statusy, id_zlecenia=0):
     zlecenia = []
     for i in range(number):
-        id = id
         phone_number = f'+48{faker.msisdn()[3:]}'
         nazwa_zamawiajacego = ''
         if random.random() <= chance_companyPerson:
@@ -108,7 +108,7 @@ def generate_zlecenia(number, dict_placowki, dict_gabaryty, dict_statusy, id=0):
         data_zakonczenia = str(data_zakonczenia)
         
         zlecenia.append({
-            'ID_zlecenia': id,
+            'ID_zlecenia': id_zlecenia,
             'Nazwa_zamawiajacego': nazwa_zamawiajacego,
             'Tel_zamawiajacego': phone_number,
             'Adres_nadania': adres_nadania,
@@ -122,17 +122,19 @@ def generate_zlecenia(number, dict_placowki, dict_gabaryty, dict_statusy, id=0):
             'Data_przyjecia': data_przyjecia,
             'Data_zakoczenia': data_zakonczenia
         })
+        id_zlecenia += 1
 
-    return zlecenia
+    return zlecenia, id_zlecenia
 
-def generate_transportyzlecenia(placowki, zlecenia, statusy, id_transportu=0):
+def generate_transportyzlecenia(placowki, zlecenia, statusy, id_zlecenia=0, id_transportu=0):
     transportyzlecenia = []
-    id_trasnportu = id_transportu
-    for i in range(len(zlecenia)):
+    i = id_zlecenia
+    for i in range(id_zlecenia, len(zlecenia)):
         # jeśli nie wykonaliśmy danego zlecenia (wcale lub jeszcze nie wykonaliśmy), nie ma ono trasy
         if statusy[zlecenia[i]['Status']]['Opis_statusu'] in ['Anulowane', 'Przyjęte']:
             continue
-        id_zlecenia = i
+
+        # poczatek zlecenia - pierwszy transport
         data_poczatek = datetime.datetime.strptime(zlecenia[i]['Data_przyjecia'], '%Y-%m-%d')
         end_date = datetime.datetime.strptime(zlecenia[i]['Data_zakoczenia'], '%Y-%m-%d')
         data_koniec = faker.date_between(start_date=data_poczatek, end_date=end_date)
@@ -141,59 +143,61 @@ def generate_transportyzlecenia(placowki, zlecenia, statusy, id_transportu=0):
         dokad = random.choice(list(placowki_dystr_id))
         placowki_dystr_id.remove(dokad)
         transportyzlecenia.append({
-            'ID_zlecenia': id_zlecenia,
-            'ID_transportu': id_trasnportu,
+            'ID_zlecenia': i,
+            'ID_transportu': id_transportu,
             'Data_poczatek': str(data_poczatek)[:10],
             'Data_koniec': str(data_koniec)[:10],
             'Skad': skad,
             'Dokad': dokad
         })
+        id_transportu += 1
 
+        # losowa ilosc nastepnych transportow
         for j in range(random.randint(0, len(placowki_dystr_id)-1)):
-            id_zlecenia = i
-            id_trasnportu += 1
             data_poczatek = data_koniec
             data_koniec = faker.date_between(start_date=data_poczatek, end_date=end_date)
             skad = dokad
             dokad = random.choice(list(placowki_dystr_id))
             placowki_dystr_id.remove(dokad)
             transportyzlecenia.append({
-                'ID_zlecenia': id_zlecenia,
-                'ID_transportu': id_trasnportu,
+                'ID_zlecenia': i,
+                'ID_transportu': id_transportu,
                 'Data_poczatek': str(data_poczatek)[:10],
                 'Data_koniec': str(data_koniec)[:10],
                 'Skad': skad,
                 'Dokad': dokad
             })
-        # jeśli zlecenie trwa, zapisujemy tylko etapy już przejechane
+            id_transportu += 1
+
+        # jeśli zlecenie trwa, nie generujemy ostatniego etapu
         if statusy[zlecenia[i]['Status']]['Opis_statusu'] != 'Zakończone':
             continue
 
-        id_zlecenia = id_zlecenia
-        id_trasnportu += 1
+        # ostatni etap
         data_poczatek = data_koniec
         data_koniec = end_date
         skad = dokad
         dokad = 'NULL'
         transportyzlecenia.append({
-            'ID_zlecenia': id_zlecenia,
-            'ID_transportu': id_trasnportu,
+            'ID_zlecenia': i,
+            'ID_transportu': id_transportu,
             'Data_poczatek': str(data_poczatek)[:10],
             'Data_koniec': str(data_koniec)[:10],
             'Skad': skad,
             'Dokad': dokad
         })
-        id_trasnportu+=1
+        id_transportu += 1
 
 
-    return transportyzlecenia
+    return transportyzlecenia, id_transportu
 
-
-def generate_DzC(transportyzlecenia, number_of_trucks, id_transportu=0):
+def generate_DzC_and_DoA(transportyzlecenia, number_of_trucks, id_transportu=0):
     DzC = []
-    for i in range(len(transportyzlecenia)):
-        id_transportu = id_transportu
+    DoA = []
+    for i in range(id_transportu, len(transportyzlecenia)):
         id_ciezarowki = random.randint(0, number_of_trucks)
+
+        # create DzC
         zuzycie_paliwa = random.randint(3, 1000)
         typ_paliwa = random.choice(fuelTypesList)
         # data from Data_koniec and random hours
@@ -203,7 +207,7 @@ def generate_DzC(transportyzlecenia, number_of_trucks, id_transportu=0):
         emisja_tlenkow_siarki = round(0.05 + random.random()*10, 2)
 
         DzC.append({
-            'ID transportu': id_transportu,
+            'ID transportu': i,
             'ID ciezarowki': id_ciezarowki,
             'Zuzycie paliwa': zuzycie_paliwa,
             'Typ paliwa': typ_paliwa,
@@ -213,33 +217,26 @@ def generate_DzC(transportyzlecenia, number_of_trucks, id_transportu=0):
             'Emisja tlenkow siarki': emisja_tlenkow_siarki
         })
 
-    return DzC
-
-def generate_DoA(transportyzlecenia, number_of_trucks, start_id=0):
-    DoA = []
-    for i in range(len(transportyzlecenia)):
-        if random.random() > chance_truckMalfunction:
+        # create DoA
+        if random.random() < chance_truckMalfunction:
             continue
-
         malfunctions_per_truck = random.randint(0, maxMalfunctionsPerTruck)
         for _ in range(malfunctions_per_truck):
-            id_transportu = i+start_id
-            id_ciezarowki = random.randint(0, number_of_trucks)
             # data from Data_koniec and random hours
             data_awarii = datetime.datetime.strptime(transportyzlecenia[i]['Data_koniec'], '%Y-%m-%d') + datetime.timedelta(hours=random.randint(1, 24), minutes=random.randint(1, 60), seconds=random.randint(1, 60))
 
             DoA.append({
-                'ID transportu': id_transportu,
+                'ID transportu': i,
                 'ID ciezarowki': id_ciezarowki,
                 'Data awarii': str(data_awarii),
                 'Kod awarii': ''.join(random.choices(string.ascii_uppercase + string.digits, k=random.randint(2, 5)))
             })
 
-    return DoA
+    return DzC, DoA
 
-def export_to_csv(data, filename):
+def export_to_csv(data, path, filename):
         keys = data[0].keys()
-        with open(filename, 'w', newline='', encoding='utf-8') as file:
+        with open(f'{path}/{filename}', 'w', newline='', encoding='utf-8') as file:
             writer = csv.DictWriter(file, fieldnames=keys)
             writer.writeheader()
             writer.writerows(data)
@@ -247,79 +244,85 @@ def export_to_csv(data, filename):
 ############################################################
 
 if __name__ == "__main__":
-    # now in T1
-    numberOf_gabaryty = 10
-    gabaryty = generate_gabaryty(numberOf_gabaryty)
+    # id counters
+    id_zlecenia = 0
+    id_gabarytu = 0
+    id_statusu = 0
+    id_placowki = 0
+    id_transportu = 0
 
-    numberOf_pno = 10
-    numberOf_dytryb = 10
-    placowki = generate_placowki(numberOf_pno, numberOf_dytryb)
+    # set number of data to be generated
+    numberOf_gabaryty_t1 = 10
+    numberOf_pno_t1 = 10
+    numberOf_dystryb_t1 = 10
+    numberOf_zlecenia_t1 = 10
 
-    statusy = generate_statusy()
+    numberOf_gabaryty_t2 = 10
+    numberOf_pno_t2 = 5
+    numberOf_dystryb_t2 = 1
+    numberOf_zlecenia_t2 = 5
+    numberOfTrucks = 10
+
+    # generating in T1 time moment
+    gabaryty, id_gabarytu = generate_gabaryty(numberOf_gabaryty_t1, id_gabarytu=0)
+    placowki, id_placowki = generate_placowki(numberOf_pno_t1, numberOf_dystryb_t1, id_placowki=0)
+    statusy, id_statusu = generate_statusy()
+    zlecenia, id_zlecenia_t1 = generate_zlecenia(numberOf_zlecenia_t1, placowki, gabaryty, statusy,  id_zlecenia=0)
+    transportyzlecenia, id_transportu_t1 = generate_transportyzlecenia(placowki, zlecenia, statusy, id_zlecenia=0, id_transportu=0)
+    DzC, DoA = generate_DzC_and_DoA(transportyzlecenia, numberOfTrucks, id_transportu=0)
     
-    numberOf_zlecenia = 10
-    zlecenia = generate_zlecenia(numberOf_zlecenia, placowki, gabaryty, statusy)
-    #print(json.dumps(zlecenia, indent=4))
+    # save to CSV files - t1
+    if not os.path.exists('./t1'):
+        os.mkdir('./t1')
+    export_to_csv(gabaryty,'./t1','gabaryty.csv')
+    export_to_csv(placowki,'./t1','placowki.csv')
+    export_to_csv(statusy,'./t1','statusy.csv')
+    export_to_csv(zlecenia,'./t1','zlecenia.csv')
+    export_to_csv(transportyzlecenia,'./t1','transportyzlecenia.csv')
+    export_to_csv(DzC,'./t1','DzC.csv')
+    export_to_csv(DoA,'./t1','DoA.csv')
 
-    numberOf_transportyzlecenia = len(zlecenia)
-    transportyzlecenia = generate_transportyzlecenia(placowki, zlecenia, statusy)
-    #print(json.dumps(transportyzlecenia, indent=4))
 
-    numberOf_DzC = len(transportyzlecenia)
-    DzC = generate_DzC(transportyzlecenia, numberOfTrucks)
-    #print(json.dumps(DzC, indent=4))
-
-    numberOf_DoA = len(transportyzlecenia)
-    DoA = generate_DoA(transportyzlecenia, numberOfTrucks)
-    #print(json.dumps(DoA, indent=4))
-
-    export_to_csv(gabaryty, './t1/gabaryty.csv')
-    export_to_csv(placowki, './t1/placowki.csv')
-    export_to_csv(statusy, './t1/statusy.csv')
-    export_to_csv(zlecenia, './t1/zlecenia.csv')
-    export_to_csv(transportyzlecenia, './t1/transportyzlecenia.csv')
-    export_to_csv(DzC, './t1/DzC.csv')
-    export_to_csv(DoA, './t1/DoA.csv')
-
-    # now in T2
-    timeEndDate = timeEndDate_T2
-    
-    placowki2 = generate_placowki(20, 15, numberOf_pno+numberOf_dytryb)
+    # generating in t2 time moment
+    placowki2, id_placowki = generate_placowki(numberOf_pno_t2, numberOf_dystryb_t2, id_placowki=id_placowki)
     for x in placowki2:
         placowki.append(x)
     
-    zlecenia2 = generate_zlecenia(20, placowki, gabaryty, statusy, numberOf_zlecenia)
+    gabaryty2, id_gabarytu = generate_gabaryty(numberOf_gabaryty_t2, id_gabarytu = id_gabarytu)
+    for x in gabaryty2:
+        gabaryty.append(x)
+
+
+    zlecenia2, id_zlecenia_t2 = generate_zlecenia(numberOf_zlecenia_t2, placowki, gabaryty, statusy, id_zlecenia=id_zlecenia_t1)
     for x in zlecenia2:
         zlecenia.append(x)
-    
-    updated = 0
-    for i in range(len(zlecenia)):
-        if updated < maxUpdated:
-            zlecenia[i]['Adres_nadania']=zlecenia[i]['Adres_nadania'].replace('ulica', 'aleja')
-            zlecenia[i]['Adres_odbioru']=zlecenia[i]['Adres_odbioru'].replace('ulica', 'aleja')
-            updated+=1
-    #print(json.dumps(zlecenia, indent=4))
 
-    transportyzlecenia2 = generate_transportyzlecenia(placowki, zlecenia, statusy, numberOf_transportyzlecenia)
+    transportyzlecenia2, id_transportu_t2 = generate_transportyzlecenia(placowki, zlecenia, statusy, id_zlecenia=id_zlecenia_t1, id_transportu=id_transportu_t1)
     for x in transportyzlecenia2:
         transportyzlecenia.append(x)
-    #print(json.dumps(transportyzlecenia, indent=4))
-
-    DzC2 = generate_DzC(transportyzlecenia, numberOfTrucks, numberOf_DzC)
+    
+    DzC2, DoA2 = generate_DzC_and_DoA(transportyzlecenia, numberOfTrucks, id_transportu=id_transportu_t1)
     for x in DzC2:
         DzC.append(x)
-    #print(json.dumps(DzC, indent=4))
 
-    DoA2 = generate_DoA(transportyzlecenia, numberOfTrucks, numberOf_DoA)
     for x in DoA2:
         DoA.append(x)
-    #print(json.dumps(DoA, indent=4))
 
-    export_to_csv(gabaryty, './t2/gabaryty.csv')
-    export_to_csv(placowki, './t2/placowki.csv')
-    export_to_csv(statusy, './t2/statusy.csv')
-    export_to_csv(zlecenia, './t2/zlecenia.csv')
-    export_to_csv(transportyzlecenia, './t2/transportyzlecenia.csv')
-    export_to_csv(DzC, './t2/DzC.csv')
-    export_to_csv(DoA, './t2/DoA.csv')
-    
+    # minor changes: change 'ulica' to 'aleja' and 'ul.' to 'al.'
+    for x in zlecenia:
+        x['Adres_nadania'] = x['Adres_nadania'].replace('ulica', 'aleja')
+        x['Adres_nadania'] = x['Adres_nadania'].replace('ul. ', 'al. ')
+        x['Adres_odbioru'] = x['Adres_odbioru'].replace('ulica', 'aleja')
+        x['Adres_odbioru'] = x['Adres_odbioru'].replace('ul. ', 'al. ')
+
+
+    # save to CSV files - t2
+    if not os.path.exists('./t2'):
+        os.mkdir('./t2')
+    export_to_csv(gabaryty,'./t2','gabaryty.csv')
+    export_to_csv(placowki,'./t2','placowki.csv')
+    export_to_csv(statusy,'./t2','statusy.csv')
+    export_to_csv(zlecenia,'./t2','zlecenia.csv')
+    export_to_csv(transportyzlecenia,'./t2','transportyzlecenia.csv')
+    export_to_csv(DzC,'./t2','DzC.csv')
+    export_to_csv(DoA,'./t1','DoA.csv')
